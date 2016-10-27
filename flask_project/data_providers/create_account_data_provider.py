0889f9fdd1954172a04fe5f2409edcd637f97bf5
marcoprado17 <marco.pdsv@gmail.com>
2016-10-27 17:22:05 -0200

    create-account page getting data from his provider

--- flask_project/data_providers/create_account_data_provider.py
+++ flask_project/data_providers/create_account_data_provider.py
@@ -1,5 +1,25 @@
-def get_create_account_data():
-	data = {
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from footer_data_provider import get_footer_data
+
+def get_create_account_data():
+	return sample_data_0()
+
+def sample_data_0():
+	data = {
+		"page_heading_data": {
+			"path": [
+				{
+					"name": "Home",
+					"href": "/",
+				},
+				{
+					"name": "Criar conta",
+				},
+			],
+			"title": "Criar conta",
+		},
+		"footer_data": get_footer_data(),
 	}
 	return data
