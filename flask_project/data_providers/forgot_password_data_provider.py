000821ab24eab192ea16e9fb3e5b6895e0c908b8
marcoprado17 <marco.pdsv@gmail.com>
2016-10-27 18:00:56 -0200

    forgot password page getting data from his provider

--- flask_project/data_providers/forgot_password_data_provider.py
+++ flask_project/data_providers/forgot_password_data_provider.py
@@ -1,5 +1,30 @@
-def get_forgot_password_data():
-	data = {
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from footer_data_provider import get_footer_data
+from flask import url_for
+
+def get_forgot_password_data():
+	return sample_data_0()
+
+def sample_data_0():
+	data = {
+		"page_heading_data": {
+			"path": [
+				{
+					"name": "Home",
+					"href": "/",
+				},
+				{
+					"name": "Entrar",
+					"href": url_for("login"),
+				},
+				{
+					"name": "Recuperar senha",
+				},
+			],
+			"title": "Recuperar senha",
+		},
+		"footer_data": get_footer_data(),
 	}
 	return data
