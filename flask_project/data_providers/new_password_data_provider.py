c6fb062c3fdf66b2c1921733a71648e0875a4d27
marcoprado17 <marco.pdsv@gmail.com>
2016-10-28 17:52:21 -0200

    new-password page getting data from his provider

--- flask_project/data_providers/new_password_data_provider.py
+++ flask_project/data_providers/new_password_data_provider.py
@@ -1,5 +1,27 @@
-def get_new_password_data():
-	data = {
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+from header_data_provider import get_header_data
+from footer_data_provider import get_footer_data
+
+def get_new_password_data():
+	return sample_data_0()
+
+def sample_data_0():
+	data = {
+		"header_data": get_header_data(),
+		"page_heading_data": {
+			"path": [
+				{
+					"name": "Home",
+					"href": "/",
+				},
+				{
+					"name": "Definir nova senha",
+				},
+			],
+			"title": "Definir nova senha",
+		},
+		"footer_data": get_footer_data(),
 	}
 	return data
