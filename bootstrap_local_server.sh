0282d100dc2fc6b82182f7910bc2f5a6909fb91e
marcoprado17 <marco.pdsv@gmail.com>
2016-10-31 08:56:30 -0200

    Removing sudo pip install to correctly install packeges in virtual env

--- bootstrap_local_server.sh
+++ bootstrap_local_server.sh
@@ -2,8 +2,9 @@ sudo apt-get update;
 sudo apt-get install -y python python-pip python-virtualenv;
 pip install --upgrade pip;
 cd /vagrant;
-sudo virtualenv env;
+sudo virtualenv env --always-copy;
 source env/bin/activate;
-sudo pip install Flask;
+pip install Flask;
 
+# if use sudo in pip install ... the package will not be installed in the env folder
 #to init local server: python /vagrant/flask_project/app.py local
