2c2ad5c78933331769ac83d94487521f1d982fa3
marcoprado17 <marco.pdsv@gmail.com>
2016-11-05 19:16:53 -0200

    Moving products related templates to a separated file

--- flask_app/views/products.py
+++ flask_app/views/products.py
@@ -11,25 +11,25 @@ from ..data_providers.products import ProductsDataProvider
 @app.route('/produto/<int:product_id>')
 def product(product_id):
     data = ProductDataProvider().get_data(product_id)
-    return render_template('product.html', data=data)
+    return render_template('products/product.html', data=data)
 
 @app.route('/produtos/pagina/<int:page>/ordenacao/<int:sort_method>')
 def all_products(page, sort_method):
     data = ProductsDataProvider().get_all_products_data(page=page, sort_method=sort_method)
-    return render_template('products.html', data=data)
+    return render_template('products/products.html', data=data)
 
 @app.route('/produtos/categoria/<int:category_id>/pagina/<int:page>/ordenacao/<int:sort_method>')
 def products_by_category(category_id, page, sort_method):
     data = ProductsDataProvider().get_products_data_by_category(category_id=category_id, page=page, sort_method=sort_method)
-    return render_template('products.html', data=data)
+    return render_template('products/products.html', data=data)
 
 @app.route('/produtos/categoria/<int:category_id>/subcategoria/<int:subcategory_id>/pagina/<int:page>/ordenacao/<int:sort_method>')
 def products_by_category_and_subcategory(category_id, subcategory_id, page, sort_method):
     data = ProductsDataProvider().get_products_data_by_category_and_subcategory(category_id=category_id, subcategory_id=subcategory_id, page=page, sort_method=sort_method)
-    return render_template('products.html', data=data)
+    return render_template('products/products.html', data=data)
 
 @app.route('/produtos/busca/pagina/<int:page>/ordenacao/<int:sort_method>')
 def products_by_search(page, sort_method):
     q = request.args.get('q')
     data = ProductsDataProvider().get_products_data_by_search(page=page, sort_method=sort_method, q=q)
-    return render_template('products.html', data=data)
+    return render_template('products/products.html', data=data)
