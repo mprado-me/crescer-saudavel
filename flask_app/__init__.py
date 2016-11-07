9bdb18984bd26f95638a4e7bd378908ab8dec741
marcoprado17 <marco.pdsv@gmail.com>
2016-11-07 12:45:34 -0200

    Refactoring flask_app/__init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,38 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_mail import Mail
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
-db = SQLAlchemy(app)
-
 bcrypt = Bcrypt(app)
 
-mail = Mail(app)
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view =  "login"
 
-# The order of this import matter. models.py uses bcrypt and db
+mail = Mail(app)
+
+# The order of this import matter. User uses bcrypt and db
 from models.user import User
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
-import views.user_management
-import views.products
-import views.cart
-import views.blog
-import views.general
-import views.admin
+import models.user
 
-import models
+import views.admin
+import views.blog
+import views.cart
+import views.general
+import views.products
+import views.user_management
+
