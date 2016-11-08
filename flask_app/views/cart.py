ec19e1dce774bb5b95fbe5a90b25f19690162821
marcoprado17 <marco.pdsv@gmail.com>
2016-11-08 07:57:17 -0200

    Refactoring routes in cart module

--- flask_app/views/cart.py
+++ flask_app/views/cart.py
@@ -12,7 +12,9 @@ from flask_login import login_required
 @app.route('/carrinho')
 @login_required
 def cart():
+    # Getting user session identifier. Aka user_email
     user_email = session["user_id"]
+
     data = cart_data_provider.get_data(user_email=user_email)
     return render_template('cart/cart.html', cart_table_editable=True, data=data)
 
@@ -20,22 +22,30 @@ def cart():
 @app.route('/adicionar-ao-carrinho/produto/<int:product_id>/quantidade/<int:quantity>', methods=['POST'])
 @login_required
 def cart_add_product(product_id, quantity):
+    # TODO: Implement
+
     return redirect(url_for('cart'))
 
 
 @app.route('/remover-do-carrinho/produto/<int:product_id>/quantidade/<int:quantity>', methods=['POST'])
 @login_required
 def cart_remove_product(product_id, quantity):
+    # TODO: Implement
+
     return redirect(url_for('cart'))
 
 
 @app.route('/deletar-do-carrinho/produto/<int:product_id>', methods=['POST'])
 @login_required
 def cart_delete_product(product_id):
+    # TODO: Implement
+
     return redirect(url_for('cart'))
 
 
 @app.route('/deletar-tudo-do-carrinho', methods=['POST'])
 @login_required
 def cart_delete_all_products():
+    # TODO: Implement
+
     return redirect(url_for('cart'))
